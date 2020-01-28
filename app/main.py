07ccf311422c9d08d3663a0dbfd4350a74337277
jiyeub <jiyeub786@gmail.com>
2020-01-28 23:59:08 +0900

    message

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import crawler as cw
+from module.logger import logger
+
+def main():
+    logger.info("----------main()----------")
+    logger.info("start main()")
+    cw.getResultFile()
+    logger.info("end main()")
+
+if __name__ == "__main__":
+    main()
+
